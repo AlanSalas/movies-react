af18f4e6ec21280c065439ea0eb3896f184f8089
AlanSalas <dubekone@gmail.com>
2022-02-11 03:56:05 -0500

    Add transition in movie card

--- src/components/styled/movieCard.jsx
+++ src/components/styled/movieCard.jsx
@@ -5,22 +5,32 @@ import { styled } from "@mui/material/styles";
 export const MovieCardContainer = styled(Stack, {
   shouldForwardProp: (prop) => !["urlImage"].includes(prop),
 })(({ theme, urlImage }) => ({
+  borderRadius: theme.shape.borderRadius,
   position: "relative",
   padding: theme.spacing(2),
   width: "100%",
   height: "440px",
   background: urlImage ? `url(${urlImage})` : "none",
-  backgroundSize: "cover",
+  backgroundSize: "100%",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  transition: theme.transitions.create(["background-size"], {
+    duration: theme.transitions.duration.standard,
+  }),
+  "&:hover": {
+    backgroundSize: "110%",
+  },
 }));
 
 export const MovieCardFilter = styled("div")({
+  borderRadius: "inherit",
   position: "absolute",
   width: "100%",
   height: "100%",
   top: "0",
   left: "0",
   zIndex: "1",
-  backgroundColor: "rgba(0, 0, 0, .3)",
+  background: "linear-gradient(transparent, rgba(0, 0, 0, 1));",
 });
 
 export const MovieLink = styled(Link)({
